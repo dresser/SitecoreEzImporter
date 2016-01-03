0045c1c7d844da8bd2a34bcbc5f1c2291c81ac64
dresser <matthew.dresser@valtech.com>
2016-01-03 17:16:51 +0100

    Removed redundant 'Delimiter' field name reference.

--- SitecoreEzImporter/Map/CustomItems/InputColumnCollectionItem.cs
+++ SitecoreEzImporter/Map/CustomItems/InputColumnCollectionItem.cs
@@ -6,7 +6,6 @@ namespace EzImporter.Map.CustomItems
     public class InputColumnCollectionItem : CustomItem
     {
         public static readonly ID TemplateId = new ID("{1A50AD7B-C6C8-4B6D-991B-37885A662DF1}");
-        public static readonly string DelimiterConstFieldName = "Delimiter";
 
         public InputColumnCollectionItem(Item item) : base(item)
         {
