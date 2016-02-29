3f9f22e11be6793283a1cb4c8aa642a4a2d9f75c
dresser <matthew.dresser@valtech.com>
2016-03-01 00:51:42 +0100

    WIP on writing API controller for speak dialog.

---
+++ SitecoreEzImporter/Controllers/ImportController.cs
@@ -0,0 +1,27 @@
+﻿using EzImporter.Models;
+using Newtonsoft.Json;
+using Sitecore.Services.Infrastructure.Web.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace EzImporter.Controllers
+{
+    public class ImportController : ServicesApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Import(ImportModel importModel)
+        {
+            var rt = new ResultType
+            {
+                Name = "Test Person"
+            };
+            return new JsonResult<ResultType>(rt, new JsonSerializerSettings(), Encoding.UTF8, this);
+        }
+    }
+
+    public class ResultType
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ SitecoreEzImporter/Pipelines/RegisterHttpRoutes.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Pipelines;
+using System.Web.Http;
+
+namespace EzImporter.Pipelines
+{
+    public class RegisterHttpRoutes
+    {
+        public void Process(PipelineArgs args)
+        {
+            GlobalConfiguration.Configure(Configure);
+        }
+
+        protected void Configure(HttpConfiguration configuration)
+        {
+            var routes = configuration.Routes;
+            routes.MapHttpRoute("ImportApi", "sitecore/api/EzImporter/Import", new
+            {
+                controller = "Import",
+                action = "Import"
+            });
+        }
+    }
+}
