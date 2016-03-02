0e9b82215d34dc3323f6593b9aa7866640ff4eb9
dresser <matthew.dresser@valtech.com>
2016-03-02 23:48:38 +0100

    Fixed SitecoreEzImporter to correctly wire up form fields.

--- SitecoreEzImporter/sitecore/SitecoreEzImporter.js
+++ SitecoreEzImporter/sitecore/SitecoreEzImporter.js
@@ -1,13 +1,13 @@
-﻿define(["sitecore"], function (Sitecore) {
+﻿define(["sitecore"], function(Sitecore) {
     var DataImporter = Sitecore.Definitions.App.extend({
+        uploadedFiles: [],
 
-        filesUploaded: [],
-
-        initialize: function () {
+        initialize: function() {
             this.on("upload-fileUploaded", this.FileUploaded, this);
         },
 
-        UploadFiles: function () {
+        //Called by the main button. Indirectly triggers ImportData once all files uploaded
+        UploadFiles: function() {
             this.ProgressIndicator.viewModel.show();
 
             if (this.SourceFile.viewModel.totalFiles() > 0) {
@@ -16,36 +16,43 @@
                 this.MessageBar.addMessage("warning", "Please select file(s) to import");
                 this.ProgressIndicator.viewModel.hide();
             }
-
         },
 
-        ImportData: function () {
+        FileUploaded: function(model) {
+            this.uploadedFiles.push(model.itemId);
+            this.SourceFile.viewModel.refreshNumberFiles();
+            if (this.SourceFile.viewModel.globalPercentage() === 100) {
+                this.ImportData();
+            }
+        },
+
+        ImportData: function() {
             var location = this.ImportLocationTreeView.viewModel.selectedItemId();
             var language = this.TargetLanguageCombo.viewModel.selectedItemId();
             var existingItemHandling = this.ExistingItemHandling.viewModel.selectedItemId();
-            var invalidLinkHandling = this.InvalidLinkHandling.viewModel.selectedItem();
-            var csvDelimiter = this.CsvDelimiter.viewModel.value;
-            var multipleValuesSeparator = this.MultipleValuesImportSeparator.viewModel.value;
-
-            alert(location + "\n" + language + "\n" + existingItemHandling + "\n" + invalidLinkHandling + "\n" + csvDelimiter + "\n" + multipleValuesSeparator);
+            var invalidLinkHandling = this.InvalidLinkHandling.viewModel.selectedItem().itemName;
+            var csvDelimiter = this.CsvDelimiter.viewModel.text();
+            var multipleValuesSeparator = this.MultipleValuesImportSeparator.viewModel.text();
+            var mappingId = this.ExistingMapping.viewModel.selectedItemId();
             if (language == null) {
-                alert('null language');
                 this.MessageBar.addMessage("warning", "Please select language for import");
             }
-
             if (location == null) {
-                alert('null import location');
                 this.MessageBar.addMessage("warning", "Please select import location");
-                alert('null import location');
             }
+            for (var i = 0; i < this.uploadedFiles.length; i++) {
 
-            for (var i = 0; i < this.filesUploaded.length; i++) {
-
-                //var item = {
-                //    TemplateId: template,
-                //    ParentId: folder,
-                //    MediaItemId: this.filesUploaded[i]
-                //};
+                var item = {
+                    MappingId: mappingId,
+                    ImportLocationId: location,
+                    Language: language,
+                    ExistingItemHandling: existingItemHandling,
+                    InvalidLinkHandling: invalidLinkHandling,
+                    CsvDelimiter: csvDelimiter,
+                    MultipleValuesSeparator: multipleValuesSeparator,
+                    MediaItemId: this.uploadedFiles[i]
+                };
+                console.log(item);
 
                 $.ajax({
                     url: "/sitecore/api/EzImporter/Import",
@@ -53,25 +60,14 @@
                     contentType: "application/json; charset=utf-8",
                     dataType: "json",
                     context: this,
-                    success: function () {
-                        this.GetImportAudit(this.filesUploaded[i - 1]);
+                    success: function() {
+
                     },
                     data: JSON.stringify(item)
                 });
             }
 
             this.ProgressIndicator.viewModel.hide();
-        },
-
-        FileUploaded: function (model) {
-
-            this.filesUploaded.push(model.itemId);
-
-            this.SourceFile.viewModel.refreshNumberFiles();
-
-            if (this.SourceFile.viewModel.globalPercentage() === 100) {
-                this.ImportData();
-            }
         }
     });
 
