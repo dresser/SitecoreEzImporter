feccbd18a75e2ff049c251efc25a9784cbbe0dd3
dresser <matthew.dresser@valtech.com>
2016-04-03 23:15:13 +0200

    Added switch to allow XLS or XLSX file types to be read.

--- SitecoreEzImporter/DataReaders/XlsxDataReader.cs
+++ SitecoreEzImporter/DataReaders/XlsxDataReader.cs
@@ -14,11 +14,15 @@ namespace EzImporter.DataReaders
             log.AppendLine("Reading XSLX input data");
             try
             {
-                //1. Reading from a binary Excel file ('97-2003 format; *.xls)
-                //IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
-
-                //2. Reading from a OpenXml Excel file (2007 format; *.xlsx)
-                IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(args.FileStream);
+                IExcelDataReader excelReader;
+                if (args.FileExtension == "xls")
+                {
+                    excelReader = ExcelReaderFactory.CreateBinaryReader(args.FileStream);
+                }
+                else
+                {
+                    excelReader = ExcelReaderFactory.CreateOpenXmlReader(args.FileStream);
+                }
 
                 excelReader.IsFirstRowAsColumnNames = args.FirstRowAsColumnNames;
                 if (!excelReader.IsValid)
