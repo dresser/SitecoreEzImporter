9226a12ffeff1fbe47b77c5d98da98e3f2a09730
dresser <matthew.dresser@valtech.com>
2016-08-15 23:56:27 +0100

    Added empty import task class.

---
+++ SitecoreEzImporter/Tasks/Import.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Tasks;
+using Sitecore.Diagnostics;
+
+namespace EzImporter.Tasks
+{
+    public class Import
+    {
+        public void Run(Item[] items, CommandItem command, ScheduleItem schedule)
+        {
+            Log.Info("ExImporter.Tasks.Import.Run()" + command.ToString(), this);
+        }
+    }
+}
