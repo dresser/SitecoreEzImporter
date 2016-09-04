f0fe45681ee172114a643c2892cb26032e81370d
dresser <matthew.dresser@valtech.com>
2016-09-04 23:50:50 +0100

    Added support for runing data importer as a scheduled task.

--- SitecoreEzImporter/Tasks/Import.cs
+++ SitecoreEzImporter/Tasks/Import.cs
@@ -1,6 +1,12 @@
-﻿using Sitecore.Data.Items;
-using Sitecore.Tasks;
+﻿using System.Web.Hosting;
+using EzImporter.Configuration;
+using EzImporter.Pipelines.ImportItems;
+using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
+using Sitecore.Pipelines;
+using Sitecore.Tasks;
+using System;
+using System.IO;
 
 namespace EzImporter.Tasks
 {
@@ -8,7 +14,106 @@ namespace EzImporter.Tasks
     {
         public void Run(Item[] items, CommandItem command, ScheduleItem schedule)
         {
-            Log.Info("ExImporter.Tasks.Import.Run()" + command.ToString(), this);
+            var importCommand = new ImportCommandItem(command.InnerItem);
+            Log.Info(
+                "EzImporter.Tasks.Import.Run() Starting Import: "
+                + " fileName=" + importCommand.FileName
+                + " lang=" + importCommand.TargetLanguage
+                + " location=" + importCommand.ImportLocationId
+                + " importMap=" + importCommand.ImportMapId
+                + " database=" + importCommand.Database.Name
+                + " csvDelimiter=" + importCommand.CsvDelimiter
+                + " ExistingItemHandling=" + importCommand.ExistingItemHandling
+                + " InvalidLinkHandling=" + importCommand.InvalidLinkHandling
+                + " MultipleValuesImportSeparator=" + importCommand.MultipleValuesImportSeparator
+                + " TreePathValuesImportSeparator=" + importCommand.TreePathValuesImportSeparator, this);
+
+            var options = Factory.GetDefaultImportOptions();
+            if (importCommand.CsvDelimiter != null)
+            {
+                options.CsvDelimiter = new[] {importCommand.CsvDelimiter};
+            }
+            if (importCommand.ExistingItemHandling != null)
+            {
+                options.ExistingItemHandling = (ExistingItemHandling)
+                    Enum.Parse(typeof (ExistingItemHandling), importCommand.ExistingItemHandling);
+            }
+            if (importCommand.InvalidLinkHandling != null)
+            {
+                options.InvalidLinkHandling = (InvalidLinkHandling)
+                    Enum.Parse(typeof (InvalidLinkHandling), importCommand.InvalidLinkHandling);
+            }
+            if (importCommand.MultipleValuesImportSeparator != null)
+            {
+                options.MultipleValuesImportSeparator = importCommand.MultipleValuesImportSeparator;
+            }
+            if (importCommand.TreePathValuesImportSeparator != null)
+            {
+                options.TreePathValuesImportSeparator = importCommand.TreePathValuesImportSeparator;
+            }
+            if (string.IsNullOrWhiteSpace(importCommand.FileName))
+            {
+                Log.Error(
+                    "EzImporter.Tasks.Import.Run() - Import Error: File not specified",
+                    this);
+                return;
+            }
+            string fileName;
+            if (File.Exists(importCommand.FileName))
+            {
+                fileName = importCommand.FileName;
+            }
+            else
+            {
+                fileName = HostingEnvironment.MapPath(importCommand.FileName);
+                if (!File.Exists(fileName))
+                {
+                    Log.Error(
+                        "EzImporter.Tasks.Import.Run() - Import Error: File not found (" + importCommand.FileName + ")",
+                        this);
+                    return;
+                }
+            }
+            var extension = GetFileExtension(fileName);
+            if (extension == null)
+            {
+                Log.Error(
+                    "EzImporter.Tasks.Import.Run() - Import Error: Unknown file extension (" + importCommand.FileName +
+                    ")", this);
+                return;
+            }
+            var stream = new StreamReader(fileName);
+            var args = new ImportItemsArgs
+            {
+                Database = importCommand.ImportDatabase,
+                FirstRowAsColumnNames = importCommand.FirstRowAsColumnNames,
+                FileExtension = extension,
+                FileStream = stream.BaseStream,
+                RootItemId = importCommand.ImportLocationId,
+                TargetLanguage = importCommand.TargetLanguage,
+                Map = Map.Factory.BuildMapInfo(importCommand.ImportMapId),
+                ImportOptions = options
+            };
+            try
+            {
+                CorePipeline.Run("importItems", args);
+                Log.Info("EzImporter.Tasks.Import.Run() Import Finished " + args.Statistics, this);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("EzImporter.Tasks.Import.Run() - Import Error: " + ex, this);
+                throw;
+            }
+        }
+
+        private string GetFileExtension(string fileName)
+        {
+            var index = fileName.LastIndexOf(".");
+            if (index > -1 && index < (fileName.Length - 1))
+            {
+                return fileName.Substring(index + 1);
+            }
+            return "";
         }
     }
 }
---
+++ SitecoreEzImporter/Tasks/ImportCommandItem.cs
@@ -0,0 +1,116 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Globalization;
+
+namespace EzImporter.Tasks
+{
+    public class ImportCommandItem : CustomItem
+    {
+        public ImportCommandItem(Item item) : base(item)
+        {
+
+        }
+
+        public string FileName
+        {
+            get { return InnerItem["FileName"]; }
+        }
+
+        public Database ImportDatabase
+        {
+            get
+            {
+                var database = Sitecore.Configuration.Factory.GetDatabase(InnerItem["Database"]);
+                return database;
+            }
+        }
+
+        public bool FirstRowAsColumnNames
+        {
+            get { return InnerItem["FirstRowAsColumnNames"] == "1"; }
+        }
+
+        public ID ImportLocationId
+        {
+            get
+            {
+                ID id;
+                if (ID.TryParse(InnerItem["ImportLocation"], out id))
+                {
+                    return id;
+                }
+                return null;
+            }
+        }
+
+        public Language TargetLanguage
+        {
+            get
+            {
+                ID id;
+                if (!ID.TryParse(InnerItem["TargetLanguage"], out id))
+                {
+                    return null;
+                }
+                var langItem = Database.GetItem(id);
+                if (langItem == null)
+                {
+                    return null;
+                }
+                return Language.Parse(langItem.Name);
+            }
+        }
+
+        public ID ImportMapId
+        {
+            get
+            {
+                ID id;
+                if (ID.TryParse(InnerItem["ImportMap"], out id))
+                {
+                    return id;
+                }
+                return null;
+            }
+        }
+
+        public string CsvDelimiter
+        {
+            get { return InnerItem["CsvDelimiter"]; }
+        }
+
+        public string ExistingItemHandling
+        {
+            get { return GetDropDownItemValue(InnerItem, "ExistingItemHandling"); }
+        }
+
+        public string InvalidLinkHandling
+        {
+            get { return GetDropDownItemValue(InnerItem, "InvalidLinkHandling"); }
+        }
+
+        private static string GetDropDownItemValue(Item item, string fieldName)
+        {
+            ID id;
+            if (ID.TryParse(item[fieldName], out id))
+            {
+                var dropDownItem = item.Database.GetItem(id);
+                if (dropDownItem != null)
+                {
+                    return dropDownItem["Name"];
+                }
+            }
+            return null;
+        }
+
+        public string MultipleValuesImportSeparator
+        {
+            get { return InnerItem["MultipleValuesImportSeparator"]; }
+        }
+
+        public string TreePathValuesImportSeparator
+        {
+            get { return InnerItem["TreePathValuesImportSeparator"]; }
+        }
+    }
+}
