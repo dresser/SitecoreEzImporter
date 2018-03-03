21c6101de4454927aa32cc2a6a56bada7722abc8
Matthew Dresser <Matthew.Dresser@valtech.co.uk>
2018-03-03 23:45:52 +0000

    Improved error messaging/exception handling for Import controller action.

--- SitecoreEzImporter/Controllers/ImportController.cs
+++ SitecoreEzImporter/Controllers/ImportController.cs
@@ -22,53 +22,71 @@ namespace EzImporter.Controllers
         {
             var database = Sitecore.Configuration.Factory.GetDatabase("master");
             var languageItem = database.GetItem(importModel.Language);
-            var uploadedFile = (MediaItem)database.GetItem(importModel.MediaItemId);
+            var uploadedFile = (MediaItem) database.GetItem(importModel.MediaItemId);
             if (uploadedFile == null)
             {
                 return new JsonResult<ImportResultModel>(null, new JsonSerializerSettings(), Encoding.UTF8, this);
             }
-            var args = new ImportItemsArgs
-            {
-                Database = database,
-                FileExtension = uploadedFile.Extension.ToLower(),
-                FileStream = uploadedFile.GetMediaStream(),
-                RootItemId = new ID(importModel.ImportLocationId),
-                TargetLanguage = Sitecore.Globalization.Language.Parse(languageItem.Name),
-                Map = Map.Factory.BuildMapInfo(new ID(importModel.MappingId)),
-                ImportOptions = new ImportOptions
-                {
-                    CsvDelimiter = new[] { importModel.CsvDelimiter },
-                    ExistingItemHandling =
-                        (ExistingItemHandling)
-                            Enum.Parse(typeof(ExistingItemHandling), importModel.ExistingItemHandling),
-                    InvalidLinkHandling =
-                        (InvalidLinkHandling)
-                            Enum.Parse(typeof(InvalidLinkHandling), importModel.InvalidLinkHandling),
-                    MultipleValuesImportSeparator = importModel.MultipleValuesSeparator,
-                    TreePathValuesImportSeparator = @"\",
-                    FirstRowAsColumnNames = importModel.FirstRowAsColumnNames
-                }
-            };
+
             ImportResultModel result;
             try
             {
-                Sitecore.Diagnostics.Log.Info(string.Format("EzImporter: mappingId:{0} mediaItemId:{1} firstRowAsColumnNames:{2}", importModel.MappingId, importModel.MediaItemId, args.ImportOptions.FirstRowAsColumnNames), this);
+                var args = new ImportItemsArgs
+                {
+                    Database = database,
+                    FileExtension = uploadedFile.Extension.ToLower(),
+                    FileStream = uploadedFile.GetMediaStream(),
+                    RootItemId = new ID(importModel.ImportLocationId),
+                    TargetLanguage = Sitecore.Globalization.Language.Parse(languageItem.Name),
+                    Map = Map.Factory.BuildMapInfo(new ID(importModel.MappingId)),
+                    ImportOptions = new ImportOptions
+                    {
+                        CsvDelimiter = new[] {importModel.CsvDelimiter},
+                        MultipleValuesImportSeparator = importModel.MultipleValuesSeparator,
+                        TreePathValuesImportSeparator = @"\",
+                        FirstRowAsColumnNames = importModel.FirstRowAsColumnNames
+                    }
+                };
+                args.ImportOptions.ExistingItemHandling = (ExistingItemHandling)
+                    Enum.Parse(typeof(ExistingItemHandling), importModel.ExistingItemHandling);
+                args.ImportOptions.InvalidLinkHandling = (InvalidLinkHandling)
+                    Enum.Parse(typeof(InvalidLinkHandling), importModel.InvalidLinkHandling);
+
+                Sitecore.Diagnostics.Log.Info(
+                    string.Format("EzImporter: mappingId:{0} mediaItemId:{1} firstRowAsColumnNames:{2}",
+                        importModel.MappingId, importModel.MediaItemId, args.ImportOptions.FirstRowAsColumnNames),
+                    this);
                 args.Timer.Start();
                 CorePipeline.Run("importItems", args);
                 args.Timer.Stop();
                 if (args.Aborted)
                 {
-                    result = new ImportResultModel { HasError = true, Log = args.Statistics.ToString(), ErrorMessage = args.Message, ErrorDetail = args.ErrorDetail };
+                    result = new ImportResultModel
+                    {
+                        HasError = true,
+                        Log = args.Statistics.ToString(),
+                        ErrorMessage = args.Message,
+                        ErrorDetail = args.ErrorDetail
+                    };
                 }
                 else
                 {
-                    result = new ImportResultModel { Log = args.Statistics.ToString() + " Duration: " + args.Timer.Elapsed.ToString("c") };
+                    result = new ImportResultModel
+                    {
+                        Log = args.Statistics.ToString() + " Duration: " + args.Timer.Elapsed.ToString("c")
+                    };
                 }
             }
             catch (Exception ex)
             {
-                result = new ImportResultModel { HasError = true, ErrorMessage = ex.Message, ErrorDetail = ex.ToString() };
+                result = new ImportResultModel
+                {
+                    HasError = true,
+                    ErrorMessage = ex.Message,
+                    ErrorDetail = ex.ToString()
+                };
             }
+
             return new JsonResult<ImportResultModel>(result, new JsonSerializerSettings(), Encoding.UTF8, this);
         }
 
